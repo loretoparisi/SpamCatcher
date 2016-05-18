af3bd6c45da91c92119e8a5e1c082bc0e5ac5270
Adrian R. Bazaga <alu0100826456@ull.edu.es>
2016-05-18 18:13:50 +0100

    AddAdded initial code to show the result

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready( function () {
+  $("#btnCalcular").click( function () {
+	  $('#resultadoClasificacion').html("<b>Result</b>: hola");
+  });
+  
+  $("#btnLimpiar").click( function () {
+	  $('#resultadoClasificacion').html("");
+  });
+});
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+const MonkeyLearn = require('monkeylearn');
+ 
+var ml = new MonkeyLearn('d8c3a3020c67ec7ecdd07d41440262e054a73642');
+var module_id = 'cl_iSNkahnf';
 
 app.set('port', (process.env.PORT || 5000));
 
